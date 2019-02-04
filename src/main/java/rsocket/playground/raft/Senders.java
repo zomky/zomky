becc617143bff9642e4494c4915c46afc2f18ba5
pmackowski <pawel.piotr.mackowski@gmail.com>
2019-02-04 21:15:47 +0100

    Senders

---
+++ src/main/java/rsocket/playground/raft/Senders.java
@@ -0,0 +1,81 @@
+package rsocket.playground.raft;
+
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Senders {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Senders.class);
+
+    private Node node;
+
+    private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+
+    private ConcurrentMap<Integer, Sender> senders = new ConcurrentHashMap<>();
+
+    public Senders(Node node, List<Integer> clientPorts) {
+        clientPorts.forEach(clientPort -> {
+            senders.put(clientPort, Sender.unavailableSender(clientPort));
+        });
+        this.node = node;
+    }
+
+    public void start() {
+        executorService.scheduleAtFixedRate(() -> {
+            senders.values().stream().filter(Sender::isNotAvailable).forEach(sender -> {
+                int nodeId = sender.getNodeId();
+                try {
+                    RSocket rSocket = RSocketFactory.connect()
+                            .transport(TcpClientTransport.create(nodeId))
+                            .start()
+                            .block();
+
+                    rSocket.onClose().subscribe(v -> {
+                        doUnavailableSender(nodeId);
+                    });
+                    doAvailableSender(nodeId, rSocket);
+                } catch (Exception e) { // TODO more specific exceptions
+                    LOGGER.debug("Node {} has no access to {}", node.nodeId, nodeId, e);
+                }
+            });
+        }, 0, 100, TimeUnit.MILLISECONDS);
+    }
+
+    public void stop() {
+        executorService.shutdown();
+    }
+
+
+    Flux<Sender> availableSenders() {
+        return Flux.create(emitter -> {
+            senders.values().stream().filter(Sender::isNotAvailable).forEach(emitter::next);
+            // lack of emitter.complete() is intentional
+        });
+    }
+
+    Flux<Sender> unavailableSenders() {
+        return Flux.defer(() -> Flux.fromIterable(senders.values())).filter(Sender::isNotAvailable);
+    }
+
+    private void doUnavailableSender(int nodeId) {
+        Sender sender = Sender.unavailableSender(nodeId);
+        LOGGER.warn("Node {} has no longer access to {}", node.nodeId, nodeId);
+        senders.put(nodeId, sender);
+        node.senderUnavailable(sender);
+    }
+
+    private void doAvailableSender(int nodeId, RSocket rSocket) {
+        LOGGER.info("Node {} has access to {}", node.nodeId, nodeId);
+        Sender sender = Sender.availableSender(nodeId, rSocket);
+        senders.put(nodeId, sender);
+        node.senderAvailable(sender);
+    }
+
+}
