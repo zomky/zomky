2b1a50fae01a483cdb986d103b91c5d164b12a9d
pmackowski <pawel.piotr.mackowski@gmail.com>
2019-07-25 00:20:30 +0200

    [gossip] playground

--- src/test/java/io/github/pmackowski/rsocket/raft/integration/gossip/GossipTest.java
+++ src/test/java/io/github/pmackowski/rsocket/raft/integration/gossip/GossipTest.java
@@ -19,145 +19,186 @@ import reactor.netty.udp.UdpClient;
 import reactor.netty.udp.UdpInbound;
 import reactor.netty.udp.UdpOutbound;
 import reactor.netty.udp.UdpServer;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GossipTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GossipTest.class);
 
-    private static LoopResources loopResources = LoopResources.create("echo-test-udp");
-
     private static class GossipNode {
 
-       private int incarnationNumber;
+        private long probePeriod = 500;
+        private long probeInterval = probePeriod * 3;
 
-       Publisher<Void> onPing(UdpInbound udpInbound, UdpOutbound udpOutbound) {
+        private volatile long delay = 1000;
+
+        private int nodeId;
+        private int incarnationNumber;
+        private Connection connection;
+        private LoopResources loopResources;
+
+        private List<Gossip> gossips = new CopyOnWriteArrayList<>();
+
+        public GossipNode(int nodeId) {
+            this.nodeId = nodeId;
+            this.incarnationNumber = 0;
+            this.loopResources = LoopResources.create("gossip-"+nodeId);
+            this.connection = UdpServer.create()
+                    .port(nodeId)
+                    .handle(this::onPing)
+                    .bindNow(Duration.ofSeconds(1));
+        }
+
+        Publisher<Void> onPing(UdpInbound udpInbound, UdpOutbound udpOutbound) {
             return udpInbound.receiveObject()
-                    .doOnNext(o -> LOGGER.info("OnPing received"))
-                    .map(o -> { // Transforms the datagram.
-                        if (o instanceof DatagramPacket) {
-                            DatagramPacket p = (DatagramPacket) o;
+                    .cast(DatagramPacket.class)
+                    .flatMap(datagramPacket -> {
+                        LOGGER.info("[Node {}] onPing", nodeId);
+                        Ping ping = toPing2(datagramPacket);
+                        Ack ack = Ack.newBuilder().setNodeId(2).build();
+//                        if (ping.getNodeId() == nodeId) {
+                        if (!ping.getReq()) {
+                            Duration delayDur = Duration.ofMillis(delay);
+                            delay = delay - 300;
+                            LOGGER.info("[Node {}] DIRECT PROBE. Delay {}", nodeId, delayDur);
+//                            return udpOutbound.sendByteArray(Mono.just(ack.toByteArray()));
+                            return Mono.just(1)
+                                        .delayElement(delayDur)
+                                        .then(udpOutbound.sendObject(new DatagramPacket(Unpooled.copiedBuffer(ack.toByteArray()), datagramPacket.sender()))
+                                        .then());
+                        } else {
+                            LOGGER.info("[Node {}] INDIRECT PROBE to {}", nodeId, ping.getNodeId());
 
-                            try {
-                                final Ping ping = Ping.parseFrom(NettyUtils.toByteArray(p.content().retain()));
-                                LOGGER.info("Received ping {}", ping);
-                                Ack ack = Ack.newBuilder()
-                                        .addGossips(Gossip.newBuilder().setIncarnation(32).build())
-                                        .build();
-                                return new DatagramPacket(Unpooled.copiedBuffer(ack.toByteArray()), p.sender());
-                            } catch (InvalidProtocolBufferException e) {
-                                throw new RuntimeException(e);
-                            }
-                        }
-                        else {
-                            return Mono.error(new Exception("Unexpected type of the message: " + o));
+                            return client(ping.getNodeId())
+                                        .flatMap(connection -> connection
+                                                .outbound()
+//                                                .sendByteArray(Mono.just(ping.toByteArray()))
+                                                .sendObject(Unpooled.copiedBuffer(Ping.newBuilder(ping).setReq(false).build().toByteArray()))
+                                                .then(connection.inbound()
+                                                        .receiveObject()
+                                                        .cast(DatagramPacket.class)
+                                                        .map(this::toAck)
+                                                        .next()
+                                                        .timeout(Duration.ofMillis(probePeriod))
+                                                        .flatMap(o -> udpOutbound
+                                                                .sendObject(new DatagramPacket(Unpooled.copiedBuffer(ack.toByteArray()), datagramPacket.sender()))
+                                                                .then()
+                                                        )
+                                                        .onErrorResume(throwable -> {
+                                                            LOGGER.info("[Node {}] InDirect probe to {} failed!", this.nodeId, ping.getNodeId());
+                                                            return Mono.empty();
+                                                        })
+                                                ).then()
+                                        );
+
                         }
                     })
-//                    .delayElements(Duration.ofMillis(200))
-                    .flatMap(udpOutbound::sendObject)
+//                    .delayElements(Duration.ofMillis(10))
                     .log("udp-server")
                     .then();
         }
 
-        Publisher<Void> pingReq(UdpOutbound udpOutbound) {
-            return Mono.empty();
+        Publisher<Void> ping(int nodeId, Integer ... indirectNodeIds) {
+            Ping ping = Ping.newBuilder().setNodeId(nodeId).build();
+            return client(nodeId)
+                .flatMap(clientConnection -> clientConnection
+                    .outbound()
+                    .sendByteArray(Mono.just(ping.toByteArray()))
+                    .then(clientConnection.inbound()
+                            .receiveObject()
+                            .doOnNext(i -> LOGGER.info("[Node {}] receiving ack from {}", this.nodeId, nodeId))
+                            .cast(DatagramPacket.class)
+                            .map(this::toAck)
+                            .next()
+                            .timeout(Duration.ofMillis(probePeriod))
+                            .then()
+                    )
+                    .then()
+                    .onErrorResume(throwable -> {
+                        LOGGER.info("[Node {}] Direct probe to {} failed!", this.nodeId, nodeId);
+
+                        return Flux.fromArray(indirectNodeIds)
+                                .flatMap(this::client)
+                                .flatMap(connection2 -> connection2
+                                .outbound()
+//                                .sendByteArray(Mono.just(Ping.newBuilder(ping).setReq(true).build().toByteArray()))
+                                .sendObject(Unpooled.copiedBuffer(Ping.newBuilder(ping).setReq(true).build().toByteArray()))
+                                .then(connection2.inbound()
+                                .receiveObject()
+                                .doOnNext(i -> LOGGER.info("[Node {}] {} receiving ack from {}", this.nodeId, connection2.address().getPort(), nodeId))
+                                .cast(DatagramPacket.class)
+                                .map(this::toAck)
+                                .next()
+                                .timeout(Duration.ofMillis(probeInterval - probePeriod))
+                                .onErrorResume(throwable1 -> {
+                                    LOGGER.info("[Node {}] {} failed {}", this.nodeId, connection2.address().getPort(), nodeId);
+                                    return Mono.empty();
+                                })
+                                .flatMap(o -> clientConnection
+                                                .outbound()
+                                                .sendByteArray(Mono.just(Ack.newBuilder().build().toByteArray()))
+                                                .then()
+                                        )
+                                )
+                            )
+                            .then();
+                    }))
+                    .log();
+//                    .timeout(Duration.ofMillis(probeInterval));
         }
 
-        Publisher<Void> ack(UdpInbound udpInbound, UdpOutbound udpOutbound) {
-            return Mono.empty();
+        private Ping toPing2(DatagramPacket datagramPacket) {
+            try {
+                Ping ping = Ping.parseFrom(NettyUtils.toByteArray(datagramPacket.content().retain()));
+                LOGGER.info("[Node {}] Ping received {}", nodeId, ping);
+                return ping;
+            } catch (InvalidProtocolBufferException e) {
+                throw new RuntimeException(e);
+            }
         }
 
+        private DatagramPacket toAck(DatagramPacket datagramPacket) {
+            LOGGER.info("[Node {}] ack ", nodeId);
+
+            Ack ack = Ack.newBuilder()
+                    .addGossips(Gossip.newBuilder().setIncarnation(32).build())
+                    .build();
+            return new DatagramPacket(Unpooled.copiedBuffer(ack.toByteArray()), datagramPacket.sender());
+        }
+
+        private Mono<? extends Connection> client(int port) {
+            return UdpClient.create()
+                    .port(port)
+                    .runOn(loopResources)
+                    .connect();
+        }
+
+        public void disposeNow() {
+            connection.disposeNow();
+        }
     }
 
     @Test
     public void echoTest() throws Exception {
-        GossipNode node = new GossipNode();
+        GossipNode node1 = new GossipNode(7000);
+        GossipNode node2 = new GossipNode(7001);
+        GossipNode node3 = new GossipNode(7002);
+        GossipNode node4 = new GossipNode(7003);
 
-        Connection server1 = server(node, 7000);
-        Connection server2 = server(node, 7001);
-        Connection server3 = server(node, 7002);
+        Thread.sleep(1_000);
 
+        Flux.from(node1.ping(7001, 7002, 7003)).blockLast();
 
+        Thread.sleep(1_000);
 
-        Connection client = client(7000);
-        final Ping ping = Ping.newBuilder()
-                .addGossips(Gossip.newBuilder().setIncarnation(1).setNodeId(7000).setOriginNodeId(7001).setSuspicion(Gossip.Suspicion.SUSPECT).build())
-                .addGossips(Gossip.newBuilder().setIncarnation(0).setNodeId(7002).setOriginNodeId(7001).setSuspicion(Gossip.Suspicion.CONFIRM).build())
-                .build();
-
-        client.outbound()
-                .sendByteArray(Mono.just(ping.toByteArray()))
-                .then(client.inbound()
-                        .receiveObject()
-                        .cast(DatagramPacket.class)
-                        .next()
-                        .timeout(Duration.ofMillis(100))
-                        .doOnNext(this::processAck)
-                        .then()
-                )
-                .then()
-                .onErrorResume(throwable -> {
-                    Flux<Connection> clientReqs = Flux.just(client(7001), client(7002));
-                    final Ping ping2 = Ping.newBuilder()
-                            .addGossips(Gossip.newBuilder().setIncarnation(110).setNodeId(27002).build())
-                            .build();
-                    return clientReqs.flatMap(connection -> connection.outbound()
-                                          .sendByteArray(Mono.just(ping2.toByteArray()))
-                                          .then(ddd(connection)))
-                                    .then();
-                })
-                .subscribe();
-
-        Thread.sleep(5_000);
-
-        server1.disposeNow();
-        server2.disposeNow();
-
-        client.disposeNow();
-    }
-
-    private void processAck(DatagramPacket p) {
-        try {
-            final Ack ack = Ack.parseFrom(NettyUtils.toByteArray(p.content().retain()));
-            LOGGER.info("Received ack {}", ack);
-
-        } catch (InvalidProtocolBufferException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    Mono<Void> ddd(Connection clientReq) {
-        return clientReq.inbound()
-                .receiveObject()
-                .next()
-                .doOnNext(o -> {
-                    if (o instanceof DatagramPacket) {
-                        DatagramPacket p = (DatagramPacket) o;
-                        try {
-                            final Ack ack = Ack.parseFrom(NettyUtils.toByteArray(p.content().retain()));
-                            LOGGER.info("Received ack {}", ack);
-                        } catch (InvalidProtocolBufferException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                })
-                .then();
-    }
-
-    Connection server(GossipNode node, int port) {
-        return UdpServer.create()
-                .port(port)
-                .runOn(loopResources)
-                .handle(node::onPing)
-                .bindNow(Duration.ofSeconds(1));
-    }
-
-    Connection client(int port) {
-        return UdpClient.create()
-                .port(port)
-                .runOn(loopResources)
-                .connectNow(Duration.ofSeconds(1));
+//        node1.disposeNow();
+//        node2.disposeNow();
+//        node3.disposeNow();
     }
 
 }
