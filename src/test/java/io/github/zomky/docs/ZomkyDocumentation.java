82a5adc510e323609f5c3d8288fbd3d810cd800d
pmackowski <pawel.piotr.mackowski@gmail.com>
2019-09-14 09:47:52 +0200

    [documentation] init doc

--- src/test/java/io/github/zomky/docs/ZomkyDocumentation.java
+++ src/test/java/io/github/zomky/docs/ZomkyDocumentation.java
@@ -1,10 +1,24 @@
 package io.github.zomky.docs;
 
+import io.github.zomky.Node;
+import io.github.zomky.NodeFactory;
+
 public class ZomkyDocumentation {
 
     public void overview() {
         // tag::overview[]
-        System.out.println("Zomky"); // <1>
+        Node bob = NodeFactory.receive()    // <1>
+                .nodeName("bob")
+                .port(7000)
+                .start()
+                .block();
+
+        Node alice = NodeFactory.receive()  // <2>
+                .nodeName("alice")
+                .port(7000)
+                .retryJoin(7001)
+                .start()
+                .block();
         // end::overview[]
     }
 }
